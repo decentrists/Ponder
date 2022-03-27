e9689e10d1e426a4858631d62b0c4d575a16faf9
David <dav@decentapps.eu>
2022-03-27 23:11:35 +0200

    Use temp object to be mutated in findAllDisjointGraphs()

--- src/components/pod-graph/cytoscape/utils.js
+++ src/components/pod-graph/cytoscape/utils.js
@@ -1,7 +1,3 @@
-export const haveSharedElements = (arr1, arr2) => arr1.some(item => arr2.includes(item));
-
-const removeDuplicateElements = array => [...new Set(array)];
-
 export const findSharedCategoriesAndKeywords = (podcast1, podcast2) => removeDuplicateElements([
   ...podcast1.categories.filter(category => podcast2.categories.includes(category)),
   ...podcast1.categories.filter(category => podcast2.keywords.includes(category)),
@@ -9,33 +5,53 @@ export const findSharedCategoriesAndKeywords = (podcast1, podcast2) => removeDup
   ...podcast1.keywords.filter(keyword => podcast2.categories.includes(keyword)),
 ]);
 
-const removeVisitedProperty = disjointGraphs => disjointGraphs
-  .map(graph => graph.map(({ visited, ...subscription }) => subscription));
+export const haveSharedElements = (arr1, arr2) => arr1.some(item => arr2.includes(item));
 
-export const findAllDisjointGraphs = (subscriptions, disjointGraphs) => {
-  const firstUnvisitedNode = subscriptions.find(item => item.visited !== true);
-  if (!firstUnvisitedNode) return removeVisitedProperty(disjointGraphs);
+/* @return [Array] The given `array`, omitting duplicate as well as falsy elements */
+const removeDuplicateElements = array => [...new Set(array.filter(x => x))];
+
+/* @return [<<Object>>] The findAllDisjointGraphs result mapped onto the subscriptions metadata */
+const finalizeDisjointGraphsObject = (subscriptions, disjointGraphs) => disjointGraphs
+  .map(graph => graph.map(node => subscriptions.find(subscription => subscription.subscribeUrl ===
+      node.subscribeUrl)));
+
+/* @param [<Object>] subscriptions
+   @return [<<Object>>] An array of graphs grouped by shared keywords & categories, where each
+     graph comprises an array of subscription metadata (nodes) */
+export const groupSubscriptionsBySharedKeywords = subscriptions => {
+  const nodes = subscriptions.map(subscription => ({
+    subscribeUrl: subscription.subscribeUrl,
+    keywordsAndCategories: removeDuplicateElements([
+      ...subscription.keywords, ...subscription.categories]),
+  }));
+  return finalizeDisjointGraphsObject(subscriptions, findAllDisjointGraphs(nodes));
+};
+
+/* @param [<Object>] nodes
+   @param [<<Object>>] disjointGraphs The intermediate result through recursion
+   @return [<<Object>>] An array of graph representations grouped by shared keywords & categories */
+const findAllDisjointGraphs = (nodes, disjointGraphs = []) => {
+  const firstUnvisitedNode = nodes.find(item => item.visited !== true);
+  if (!firstUnvisitedNode) return disjointGraphs;
+
   firstUnvisitedNode.visited = true;
-
-  let keywordsAndCategoriesInCommon = removeDuplicateElements([...firstUnvisitedNode.categories,
-    ...firstUnvisitedNode.keywords]);
+  let keywordsAndCategoriesInCommon = firstUnvisitedNode.keywordsAndCategories;
 
   const graph = [firstUnvisitedNode];
   let relatedPodcast;
   do {
     // eslint-disable-next-line no-loop-func
-    relatedPodcast = subscriptions.find(item => item.visited !== true
-      && haveSharedElements(keywordsAndCategoriesInCommon, [...item.keywords, ...item.categories]));
-
+    relatedPodcast = nodes.find(item => item.visited !== true
+      && haveSharedElements(keywordsAndCategoriesInCommon, item.keywordsAndCategories));
     if (!relatedPodcast) break;
 
     relatedPodcast.visited = true;
     keywordsAndCategoriesInCommon = removeDuplicateElements([...keywordsAndCategoriesInCommon,
-      ...relatedPodcast.categories, ...relatedPodcast.keywords]);
+      ...relatedPodcast.keywordsAndCategories]);
     graph.push(relatedPodcast);
   } while (relatedPodcast);
 
   disjointGraphs.push(graph);
 
-  return findAllDisjointGraphs(subscriptions, disjointGraphs);
+  return findAllDisjointGraphs(nodes, disjointGraphs);
 };
