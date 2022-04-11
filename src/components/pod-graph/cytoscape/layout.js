b80bbd2e25c034f33f6912252c99db7f913153aa
Mahdi Ghajary <mahdighajary@gmail.com>
2022-04-11 15:23:16 +0430

    put layout.js comments before properties

--- src/components/pod-graph/cytoscape/layout.js
+++ src/components/pod-graph/cytoscape/layout.js
@@ -1,29 +1,41 @@
 const layout = {
-  fit: true, // whether to fit the viewport to the graph
-  padding: 80, // the padding on fit
+  // whether to fit the viewport to the graph
+  fit: true,
+  // the padding on fit
+  padding: 80,
   name: 'dagre',
   nodeSep: 50,
   edgeSep: 80,
   rankSep: 300,
-  rankDir: 'LR', // 'TB' for top to bottom flow, 'LR' for left to right,
-  ranker: 'tight-tree', // Type of algorithm to assign a rank to each node in the
-  // input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
-  minLen() { return 1; }, // number of ranks to keep between the source and target of the edge
-  edgeWeight() { return 1; }, // higher weight edges are generally
-  // made shorter and straighter than lower weight edges
-  spacingFactor: undefined, // Applies a multiplicative factor (>0)
-  // to expand or compress the overall area that the nodes take up
-  nodeDimensionsIncludeLabels: true, // whether labels should be included
-  // in determining the space used by a node
-  animate: false, // whether to transition the node positions
-  animateFilter() { return true; }, // whether to animate specific nodes
-  // when animation is on; non-animated nodes immediately go to their final positions
-  animationDuration: 500, // duration of animation in ms if enabled
-  animationEasing: undefined, // easing of animation if enabled
-  transform(node, pos) { return pos; }, // a function that applies a transform
-  // to the final node position
-  ready() {}, // on layoutready
-  stop() {}, // on layoutstop
+  // 'TB' for top to bottom flow, 'LR' for left to right,
+  rankDir: 'LR',
+  // Type of algorithm to assign a rank to each node in the input graph.
+  // Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
+  ranker: 'tight-tree',
+  // number of ranks to keep between the source and target of the edge
+  minLen() { return 1; },
+  // higher weight edges are generally made shorter and straighter than lower weight edges
+  edgeWeight() { return 1; },
+  // Applies a multiplicative factor (>0)  to expand or compress
+  // the overall area that the nodes take up
+  spacingFactor: undefined,
+  // whether labels should be included in determining the space used by a node
+  nodeDimensionsIncludeLabels: true,
+  // whether to transition the node positions
+  animate: false,
+  // whether to animate specific nodes when animation is on;
+  // non-animated nodes immediately go to their final positions
+  animateFilter() { return true; },
+  // duration of animation in ms if enabled
+  animationDuration: 500,
+  // easing of animation if enabled
+  animationEasing: undefined,
+  // a function that applies a transform to the final node position
+  transform(node, pos) { return pos; },
+  // on layoutready
+  ready() {},
+  // on layoutstop
+  stop() {},
 };
 
 export default layout;
