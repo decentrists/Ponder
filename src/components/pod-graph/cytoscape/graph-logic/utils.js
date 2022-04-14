d0848748a3dbbf8d02e0bb78171924a28adb5b78
David <dav@decentapps.eu>
2022-04-14 17:37:35 +0200

    Fix non-JSDoc comments, add some more helper functions

--- src/components/pod-graph/cytoscape/graph-logic/utils.js
+++ src/components/pod-graph/cytoscape/graph-logic/utils.js
@@ -7,5 +7,8 @@ export const findSharedCategoriesAndKeywords = (podcast1, podcast2) => removeDup
 
 export const haveSharedElements = (arr1, arr2) => arr1.some(item => arr2.includes(item));
 
-/* @return [Array] The given `array`, omitting duplicate as well as falsy elements */
+/**
+ * @param {Array} array
+ * @returns {Array} The given `array`, omitting duplicate as well as falsy elements
+ */
 export const removeDuplicateElements = array => [...new Set(array.filter(x => x))];
