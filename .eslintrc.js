7aac1c6dd1e72bd94dc05ccf127c2a623e5e122f
Mahdi Ghajary <mahdighajary@gmail.com>
2022-04-19 23:03:49 +0430

    fix eslint versions and extraneous rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,11 @@
 module.exports = {
-  extends: ['airbnb-typescript'],
+  extends: ['airbnb-typescript', 'react-app', 'react-app/jest'],
   env: {
     browser: true,
   },
   rules: {
-    'arrow-parens': [2, 'as-needed'],
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'import/prefer-default-export': 0,
-    'react/jsx-no-bind': 0,
-    'react/static-property-placement': [2, 'static public field'],
-    'consistent-return': 0,
-    'no-console': 0,
     '@typescript-eslint/brace-style': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
-    'no-plusplus': 'off',
-    'no-underscore-dangle': 'off',
-    'operator-linebreak': 'off',
-    'no-continue': 'off',
-    'prefer-object-spread': 'off',
     'object-curly-newline': ['error', {
       ImportDeclaration: {
         consistent: true,
@@ -27,6 +14,7 @@ module.exports = {
       },
     }],
   },
+  'parser': '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     createDefaultProgram: true,
@@ -58,28 +46,10 @@ module.exports = {
       },
     },
     {
-      files: [
-        '**/*.test.js',
-        './tests/**/*',
-        '**/__mocks__/**/*.js',
-        'jest.setup.js',
-      ],
-      plugins: ['jest'],
-      env: {
-        jest: true,
-      },
-    },
-    {
       files: ['cypress/**/*'],
       plugins: ['cypress'],
       env: {
         'cypress/globals': true,
-      },
-    },
-    {
-      files: ['src/components/cytoscape/cytoscape/**/*.js'],
-      rules: {
-        'no-underscore-dangle': 0,
       },
     },
     {
