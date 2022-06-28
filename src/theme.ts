8527b6300b6a89ba9fc559e50cf49f62862ae6de
Mahdi Ghajary <46849131+mmahdigh@users.noreply.github.com>
2022-06-28 10:09:24 +0430

    add mobile cytoscape layout (#48)

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,15 @@ export const theme = createTheme({
       main: '#fff',
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   components: {
     MuiTabs: {
       styleOverrides: {
