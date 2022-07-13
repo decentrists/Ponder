3ec21cb44dc8f16c4d1133719cb4ac6b4a7ee2fa
Mahdi Ghajary <mahdighajary@gmail.com>
2022-07-13 13:02:59 +0430

    install arbundles

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  console.log('override');
+  const loaders = config.resolve;
+  loaders.fallback = {
+    path: require.resolve('path-browserify'),
+    zlib: require.resolve('browserify-zlib'),
+    stream: require.resolve('stream-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    assert: require.resolve('assert'),
+    // buffer: require.resolve('buffer/'),
+  };
+  // eslint-disable-next-line no-param-reassign
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+
+  return config;
+};
